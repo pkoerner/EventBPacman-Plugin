d66b0b6d097d63993b6827c136130455560ce2e5
dgelessus <dgelessus@users.noreply.github.com>
2019-03-28 14:42:15 +0100

    Enable the Pacman view only when a Pacman machine is loaded

--- src/main/java/de/heinzen/probplugin/pacman/PacmanPlugin.java
+++ src/main/java/de/heinzen/probplugin/pacman/PacmanPlugin.java
@@ -1,5 +1,6 @@
 package de.heinzen.probplugin.pacman;
 
+import de.prob.model.representation.Machine;
 import de.prob.statespace.Trace;
 import de.prob2.ui.plugin.ProBPlugin;
 import de.prob2.ui.plugin.ProBPluginHelper;
@@ -7,9 +8,8 @@ import de.prob2.ui.plugin.ProBPluginManager;
 import de.prob2.ui.prob2fx.CurrentTrace;
 
 import javafx.beans.value.ChangeListener;
-import javafx.event.EventHandler;
+import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
-import javafx.scene.input.KeyEvent;
 
 import org.pf4j.PluginWrapper;
 import org.slf4j.Logger;
@@ -40,27 +40,23 @@ public class PacmanPlugin extends ProBPlugin {
         PacmanGui gui = new PacmanGui(animator);
         PacmanLogic logic = new PacmanLogic(gui, animator);
 
-        final EventHandler<KeyEvent> keyListener = event -> currentTrace.set(logic.movePacman(currentTrace.get(), event.getCode()));
-
-        if (currentTrace.getCurrentState() != null && currentTrace.getCurrentState().isInitialised()) {
-            gui.createGui(currentTrace.get(), pacmanTab, keyListener);
-        }
-
-        currentTraceChangeListener = (observable, oldValue, newValue) -> {
-            //TODO: check if the pacman machine is loaded
-            if (oldValue != null && oldValue.getCurrentState() != null
-                    && newValue != null && newValue.getCurrentState() != null
-                    && !oldValue.getCurrentState().isInitialised()
-                    && newValue.getCurrentState().isInitialised()) {
-                gui.createGui(newValue, pacmanTab, keyListener);
-            } else if ( newValue != null && newValue.getCurrentState() != null
-                    && newValue.getCurrentState().isInitialised()) {
-                gui.update(newValue);
+        currentTraceChangeListener = (o, from, to) -> {
+            if (to == null) {
+                pacmanTab.setContent(new Label("No model loaded"));
+            } else if (!(to.getStateSpace().getMainComponent() instanceof Machine) || !((Machine)to.getStateSpace().getMainComponent()).getName().contains("Pacman")) {
+                pacmanTab.setContent(new Label("This is not a Pacman machine"));
+            } else if (!to.getCurrentState().isInitialised()) {
+                pacmanTab.setContent(new Label("Machine not initialized"));
+            } else if (pacmanTab.getContent() == null || pacmanTab.getContent() instanceof Label) {
+                gui.createGui(to, pacmanTab, event -> currentTrace.set(logic.movePacman(currentTrace.get(), event.getCode())));
+            } else {
+                gui.update(to);
             }
         };
 
         getProBPluginHelper().addTab(pacmanTab);
         currentTrace.addListener(currentTraceChangeListener);
+        currentTraceChangeListener.changed(currentTrace, null, currentTrace.get());
     }
 
     @Override
